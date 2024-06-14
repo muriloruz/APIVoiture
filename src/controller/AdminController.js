d89f368659eadbc1583dbe1096f4cf4e4abfdc23
muriloruz <muriloruz64@gmail.com>
2024-06-14 18:07:17 -0300

    eu desisto dessa porra

--- src/controller/AdminController.js
+++ src/controller/AdminController.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const UserModel = require("../models/user");
+router.get("/users",async (req, res) => {
+    const user = req.headers;
+    
+    const espe=JSON.stringify(user); 
+    const mau = espe.substring(18,196);
+    const userfinal = await UserModel.findOne({token:mau});
 
-router.get("/users", (req, res)=>{
-    return res.json({
-
-    });
+    return res.json({userfinal});
 });
 
 module.exports = router;
