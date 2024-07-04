239e485ae6991a57c4861cd0163abbb5799cba2c
Murilo Ruz <muriloruz64@gmail.com>
2024-07-04 18:40:55 -0300

    Algumas rotas prontas

---
+++ APIVoiture/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using APIVoiture.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIVoiture.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+    private static List<Usuario> users = new List<Usuario>();
+    private static int id = 0;
+    [HttpPost]
+    public IActionResult AdicionaUsuario([FromBody] Usuario user)
+    {
+        user.Id = id++;
+        users.Add(user);
+        return CreatedAtAction(nameof(GetSingleUser), new {id = user.Id},
+        user);
+            
+    }
+    [HttpGet("single/{id}")]
+    public IActionResult GetSingleUser(int id)
+    {
+        var u = users.FirstOrDefault(user => user.Id == id);
+        if(u== null)
+            return NotFound();
+        return Ok(u);
+    }
+    [HttpGet("mult")]
+    public IEnumerable<Usuario> GetUsuarioEmMultiplo([FromQuery]int skip=0,[FromQuery] int take = 10)
+    {
+        return users.Skip(skip).Take(take).ToList();
+    }
+    [HttpGet("all")]
+    public IEnumerable<Usuario> GetAllUsers()
+    {
+        return users;
+    }
+}
---
+++ APIVoiture/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace APIVoiture.Models;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage ="O campo nome eh obrigatorio")]
+    [StringLength(70, ErrorMessage ="Nome muito grande")]
+    public string nome { get; set; }
+    [Range(18,120,ErrorMessage = "O usuario nao pode ter mais de 120 nem menos de 18")]
+    public int idade { get; set; }
+    public string telefone  { get; set; }
+    [MinLength(8,ErrorMessage ="CEP menor que tamanho minimo")]
+    [MaxLength(8,ErrorMessage ="CEP maior que tamanho maximo")]
+    public string CEP {  get; set; }
+    [MinLength(11,ErrorMessage ="Menor que 11")]
+    [MaxLength(11,ErrorMessage ="Maior que 11")]
+    public string CPF { get; set; }
+    public string sexo { get; set; }
+    [Required(ErrorMessage ="o campo email eh obrigatorio")]
+    public string email { get; set; }
+    [Required(ErrorMessage = "o campo senha eh obrigatorio")]
+    public string senha { get; set; }
+    
+}
