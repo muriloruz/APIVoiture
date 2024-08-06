c1b6109d4d200e24555dea9e56e0c13e1331e9a1
unknown <muriloruz64@gmail.com>
2024-08-06 15:54:37 -0300

    Só falta o criar o user do vendedor

--- APIVoiture/obj/Debug/net6.0/APIVoiture.AssemblyInfo.cs
+++ APIVoiture/obj/Debug/net6.0/APIVoiture.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIVoiture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87cfe37c8b9c4fb4b4cc14f30ffaaa0f0de1d238")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIVoiture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIVoiture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
